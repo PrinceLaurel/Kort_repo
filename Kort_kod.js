6dea94abb8a7a17070bc82a3142a84077a2b0c4b
PrinceLaurel <prince.laurel@edu.huddinge.se>
2023-09-20 13:10:22 +0200

    gjort kod fil och buildDeck function

---
+++ Kort_kod.js
@@ -0,0 +1,12 @@
+let suits = ["C", "D", "H", "S"];
+let values = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+let deck = [];
+
+function buildDeck() {
+    for (let i = 0; i < suits.length; i++) {
+        for (let j = 0; j < values.length; j++) {
+            deck.push(values[j] + "-" + suits[i]);
+        }
+    }
+    console.log(deck);
+}
