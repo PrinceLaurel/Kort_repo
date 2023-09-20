377b9433557787b8f8174971fb42408a8f79f967
PrinceLaurel <prince.laurel@edu.huddinge.se>
2023-09-20 13:28:03 +0200

    la till shuffle function, + uppdaterade buildDeck func

--- Kort_kod.js
+++ Kort_kod.js
@@ -3,10 +3,19 @@ let values = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
 let deck = [];
 
 function buildDeck() {
-    for (let i = 0; i < suits.length; i++) {
-        for (let j = 0; j < values.length; j++) {
-            deck.push(values[j] + "-" + suits[i]);
+    for (let suit of suits) {
+        for (let value of values) {
+            deck.push(`${suit} ${value}`);
         }
     }
     console.log(deck);
 }
+function shuffle(deck) {
+    for (let i = deck.length - 1; i > 0; i--) {
+        let j = Math.floor(Math.random() * (i + 1));
+        [deck[i], deck[j]] = [deck[j], deck[i]];
+    }
+    return deck;
+}
+shuffle(deck);
+console.log(deck);
